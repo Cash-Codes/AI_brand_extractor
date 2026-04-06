dbcd8e3e36f704690fc227206577cd8f5005a5da
Cash-Codes <sf4cashley@gmail.com>
2026-04-06 15:43:08 +0100

    fix: migrate @MockBean to @MockitoBean

--- src/test/java/com/brandextractor/BrandExtractorApplicationTests.java
+++ src/test/java/com/brandextractor/BrandExtractorApplicationTests.java
@@ -3,12 +3,12 @@ package com.brandextractor;
 import com.google.cloud.vertexai.VertexAI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class BrandExtractorApplicationTests {
 
-    @MockBean
+    @MockitoBean
     VertexAI vertexAI;
 
     @Test
